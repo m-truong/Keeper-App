8ce9862c0ce64ef123e551927c56b8e8301a033f
Michael Truong <michael.truong@savvas.com>
2021-12-04 15:29:48 -0500

    Add package.json and core folders

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
+};
+
+export default App;
