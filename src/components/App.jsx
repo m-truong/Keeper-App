22b9dabcf15436bb591f9019e1112498d41e1292
Michael Truong <truong.michael.2013@gmail.com>
2021-12-05 20:45:32 -0500

    Create complex state object and reusable handler functions
    I added a complex React state object to store the title and content area fields. I also added a reusable handler function to handle input changes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,62 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const cl = console.log
+
+  // *** Use Complex State Object
+  const [textData, setTextData] = useState({
+    title: '',
+    content: ''
+  })
+
+  const [notesList, setNotesList] = useState([]) // CREATED ARRAY OF OBJECTS with Title/Content!
+
+  // *** Use RE-usable Function Handler
+  const handleInputChange = (e) => {
+    cl(e.target)
+
+    const { name, placeholder, value } = e.target
+
+    if (name === "title") {
+      setTextData((prevState) => {
+        return {
+          ...prevState,
+          [name]: value
+        }
+      })
+    } else if (name === "content") {
+      setTextData((prevState) => {
+        return {
+          ...prevState,
+          [name]: value
+        }
+      })
+    }
+  }
+
+  const handleSubmit = (e) => {
+    cl(`Does this occur?`)
+    setNotesList((prevNotes) => {
+      return [...prevNotes, textData]
+    })
+
+    setTextData({ title: '', content: '' })
+    e.preventDefault();
+  }
+
+  const handleDelete = (e) => {
+    cl(`Does this occur?`)
+    
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea
+        title={textData.title}
+        content={textData.content}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit} />
+      <Note handleDelete={handleDelete} key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 
-function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+function CreateArea({ title, content, handleInputChange, handleSubmit }) {
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="title"
+                    placeholder="Title"
+                    value={title}
+                    onChange={handleInputChange}
+                />
+                <textarea 
+                name="content" 
+                placeholder="Take a note..." 
+                rows="3" 
+                onChange={handleInputChange}
+                value={content}
+                />
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    );
 }
 
 export default CreateArea;
